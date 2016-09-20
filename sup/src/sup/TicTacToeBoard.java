1697d426d49bdcdd8a63a1794a9a1b5da32689cb
323827311 <323827311@C-5Q3ZJS1.tdsb.on.ca>
2016-09-20 13:15:14 -0400

    TicTacToeboard

---
+++ sup/src/sup/TicTacToeBoard.java
@@ -0,0 +1,34 @@
+package sup;
+
+/* ClassName.java
+ * TicTacToe Board
+ * Sept/20/16
+ * Aisha Rashid 
+ */
+
+
+ 
+
+public class TicTacToeBoard {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+                      
+	System.out.format("%10s %7s","|","|\n");
+	 System.out.format("%10s %7s","|","|\n");
+	 System.out.format("%10s %7s","|","|\n");
+	 System.out.format("-------------------------\n");
+	 System.out.format("%10s %7s","|","|\n");
+	 System.out.format("%10s %3s %3s","|","x","|\n");
+	 System.out.format("%10s %8s","|","| \n");
+	 System.out.format("------------------------\n");
+	 System.out.format("%10s %7s","|","|\n");
+	 System.out.format("%10s %7s","|","|\n");
+	 System.out.format("%10s %7s","|","|\n");
+
+
+}
+
+
+
+
+}
