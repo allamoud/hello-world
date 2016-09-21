800eb90c154b2df461f29bb107dc3c9aecec2e77
323827311 <323827311@C-5Q3ZJS1.tdsb.on.ca>
2016-09-21 13:12:52 -0400

    TicTacToe Rules

--- sup/src/sup/TicTacToeRules.java
+++ sup/src/sup/TicTacToeRules.java
@@ -11,10 +11,10 @@ public class TicTacToeRules {
 		     System.out.format(" 1. A grid of 9 squares is drawn.\n");
 		     System.out.format("\n");
 		     System.out.format("2.The first player draws an X in an empty square.\n");
+		     System.out.format("\n");
 		     System.out.format("3.The second player draws an O in an empty square.\n");
-		     System.out.format("4. Steps 2 and 3 are repeated until a Tic Tac pattern is created:\n");
-		     System.out.format("Pattern can be diagonal, horizontal, or vertical\n");
-		     System.out.format("5. A winning pattern is completed by drawing a line though the pattern\n");
+		     System.out.format("\n");
+		    System.out.format("4. Steps 2 and 3 are repeated until a Tic Tac pattern is created:\n");	 
+		     System.out.format("Patterns can be diagonal, horizontal, or vertical\n");
+		     System.out.format("5.A winning pattern is completed by drawing a line through the pattern\n");
 		     
-		    		 
-		
