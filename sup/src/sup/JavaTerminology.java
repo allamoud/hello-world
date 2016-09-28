735fe9424acc427a327c012c4af82524ea5a9de4
323827311 <323827311@C-5Q3ZJS1.tdsb.on.ca>
2016-09-28 13:21:34 -0400

    JavaTerminology

---
+++ sup/src/sup/JavaTerminology.java
@@ -0,0 +1,23 @@
+package sup;
+/*ClassName.java
+ * TicTacToe Rules
+ * Sept/15/16
+ * Aisha Rashid
+ */
+public class JavaTerminology {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  System.out.format("Print: Method that displays output to the screen.\n");
+System.out.format("\n");
+  System.out.format("Main:The method in the contorlling class that is automatically executed when java application is run\n");
+System.out.format("\n");  
+  System.out.format("String:A setof characters.\n");
+System.out.format("\n");
+  System.out.format("Argument:Data passed to a method for processing\n");
+System.out.format("\n");
+  System.out.format("Libary:see package\n");
+System.out.format("\n");
+
+	}
+}
+
