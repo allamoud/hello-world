72eb094a6598185c6e302a984659caa7c9d6bf34
323827311 <323827311@C-5Q3ZJS1.tdsb.on.ca>
2016-10-31 13:42:55 -0400

    rashid

--- sup/src/Rashid/QuadraticEqaution.java
+++ sup/src/Rashid/QuadraticEqaution.java
@@ -11,26 +11,27 @@ import java.util.Scanner;
 */
 public class QuadraticEqaution {
 
-public void main(String[] args) {
-// TODO Auto-generated method stub
+	public void main(String[] args) {
+		// TODO Auto-generated method stub
 
-int a,b,c;
+		int a,b,c;
 
-Scanner scan =new Scanner(System.in);
-DecimalFormat df  = new DecimalFormat ("0.00");
+		Scanner scan =new Scanner(System.in);
+		DecimalFormat df  = new DecimalFormat ("0.00");
 
 
-System.out.println("Enter Number");
- 
-a=scan.nextInt();
-b=scan.nextInt();
-c=scan.nextInt();
+		System.out.println("Enter Number");
 
-System.out.println(df.format(-1*b+(Math.sqrt(Math.pow(b,2)+(-4*(a*c)))/2*a)));
+		a=scan.nextInt();
+		b=scan.nextInt();
+		c=scan.nextInt();
 
-System.out.println(df.format(-1*b-(Math.sqrt(Math.pow(b,2)+(-4*(a*c)))/2*a)));
+		System.out.println(df.format(-1*b+(Math.sqrt(Math.pow(b,2)+(-4*(a*c)))/2*a)));
+
+		System.out.println(df.format(-1*b-(Math.sqrt(Math.pow(b,2)+(-4*(a*c)))/2*a)));
+
+		scan.close();
+	}
 
 }
-
- }
 
---
+++ sup/src/Rashid/Work1.java
@@ -0,0 +1,29 @@
+package Rashid;
+in
+/*ClassName.java
+ * takes an integer as input ,then tests it to see if it is 
+ * 
+ */
+public class Work1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     Scanner scan=new Scanner (System.in);
+     
+     int number;
+     
+     System.out.println("Type in a number");
+     
+     number=scan.nextInt();
+     if(number>0)
+     {
+    	 System.out.println("Your number is positive");
+     }
+     else
+     {
+     System.out.println("Your number is negative");
+     if
+        
+	}
+
+}
