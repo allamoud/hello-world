a92377cc3ceb0134fb3522e9b8f515a5eb334bb1
323827311 <323827311@C-5Q3ZJS1.tdsb.on.ca>
2016-10-31 13:08:11 -0400

    Quadratic and the start of order and time

---
+++ sup/src/Rashid/QuadraticEqaution.java
@@ -0,0 +1,36 @@
+package Rashid;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+/*quadraticEquation.java
+* Quadratic Equation
+* Sept/30/16
+* Aisha Rashid 
+* This program displays the quadratic formulas.
+*/
+public class QuadraticEqaution {
+
+public void main(String[] args) {
+// TODO Auto-generated method stub
+
+int a,b,c;
+
+Scanner scan =new Scanner(System.in);
+DecimalFormat df  = new DecimalFormat ("0.00");
+
+
+System.out.println("Enter Number");
+ 
+a=scan.nextInt();
+b=scan.nextInt();
+c=scan.nextInt();
+
+System.out.println(df.format(-1*b+(Math.sqrt(Math.pow(b,2)+(-4*(a*c)))/2*a)));
+
+System.out.println(df.format(-1*b-(Math.sqrt(Math.pow(b,2)+(-4*(a*c)))/2*a)));
+
+}
+
+ }
+
---
+++ sup/src/sup/Time.java
@@ -0,0 +1,49 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+
+   /*ClassName.java
+        *Time 
+        * oct/11/16
+        * Aisha Rashid
+        */
+
+public class Time {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       
+		Scanner scan=new Scanner (System.in);
+		DecimalFormat df = new DecimalFormat ("0.00");
+		
+		double design;
+		double coding;
+		double debugging;
+		double testing;
+		
+		System.out.println("Enter the amount spent on design");
+		design =scan.nextDouble();
+		
+		System.out.println("Enter the amount spent coding");
+		coding =scan.nextDouble();
+		
+		System.out.println("Enter the amount spent debugging");
+		debugging =scan.nextDouble();
+		
+		System.out.println("Enter the amount spent on testing");
+		testing =scan.nextDouble();
+		  
+		System.out.format("%10s%10s","Task","%Time");
+		
+		System.out.format("Desiging" + df.format(design\design+coding+debugging+testing)*100);
+		
+		
+		System.out.format("Coding" + df.format(design+coding+debugging+testing)*100);
+		
+		
+		System.out.format("debugging"+ df.format(
+		
+		
+	}
+
+}
