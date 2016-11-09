d5f48cd8d1fc6e9e93564cfe9cbf05528c54c049
323827311 <323827311@C-5Q3ZJS1.tdsb.on.ca>
2016-11-09 13:04:59 -0500

    Random1

---
+++ sup/src/Rashid/Random1.java
@@ -0,0 +1,36 @@
+package Rashid;
+
+import java.util.Scanner;
+
+/*
+ * 
+ * 
+ */
+public class Random1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     
+		
+		Scanner scan=new Scanner (System.in);
+		
+		       int x;
+		       
+		System.out.print ("What is your name ");
+		String a=scan.nextLine();
+		x = (int) (Math.random() *100+1);
+
+		if
+		
+		(x<=29)
+		
+			
+	  System.out.println(a+ "That is not your favrite name");
+		
+		else if
+		
+		(x>=30)
+			System.out.println(a +"That is your favrite name ");
+	}
+
+}
